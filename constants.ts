1c488748eb37680497a4b25766bbe7d5e98b42c9
studentmtk <82542013+studentmtk@users.noreply.github.com>
2024-06-09 19:57:16 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,18 +1,12 @@
 import { PublicKey } from "@solana/web3.js";
 
-// your private key here
-export const PRIVATE_KEY = "";
-
-export const TRANSFER_TO = new PublicKey(
-  "AyuS8y6dLsXXoQMkz5HLwgSaYxz8xaWgYH5asjq4VzU8"
-)
 
 export const PROGRAM_ID = new PublicKey(
-  "meRjbQXFNf5En86FXT2YPz1dQzLj4Yb3xK8u1MVgqpb"
+  "DiSLRwcSFvtwvMWSs7ubBMvYRaYNYupa76ZSuYLe6D7j"
 );
 
 export const TOKEN_ID = new PublicKey(
-  "JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"
+  "UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG"
 );
 
 export const SOLANA_RPC = "https://api.mainnet-beta.solana.com";
