22ddcd75564fa8e0a0726e214c8fe126d6bfbde1
studentmtk <82542013+studentmtk@users.noreply.github.com>
2024-06-09 19:59:16 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import { utils, Program, getProvider, AnchorProvider, Wallet, BN } from "@project-serum/anchor";
-import { Connection, Keypair, ParsedAccountData, PublicKey, Transaction } from "@solana/web3.js";
-import { ASSOCIATED_TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction, createTransferInstruction, getAssociatedTokenAddress, getOrCreateAssociatedTokenAccount, transferInstructionData } from "@solana/spl-token";
-
-import { PRIVATE_KEY, PROGRAM_ID, SOLANA_RPC, TOKEN_ID, TRANSFER_TO } from "./constants";
+import { utils, Program, AnchorProvider, Wallet, BN } from "@project-serum/anchor";
+import { Connection, Keypair, PublicKey, Transaction, ComputeBudgetProgram } from "@solana/web3.js";
+import { createAssociatedTokenAccountInstruction, getAssociatedTokenAddress } from "@solana/spl-token";
+import { PROGRAM_ID, SOLANA_RPC, TOKEN_ID } from "./constants";
 import { IDL } from "./idl";
 import invariant from "tiny-invariant";
 import axios from "axios";
 import bs58 from 'bs58';
+import fs from 'fs';
 
 const SOLANA_CONNECTION = new Connection(SOLANA_RPC, "confirmed");
 
 export const getClaimInfo = async(address: string) => {
-  const info = await axios.get(`https://worker.jup.ag/jup-claim-proof/JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN/${address}`);
+  const info = await axios.get(`https://worker.jup.ag/jup-claim-proof/UPTx1d24aBWuRgwxVnFmX4gNraj3QGFzL3QqBgxtWQG/${address}`);
   return info.data;
 }
 
@@ -49,23 +49,8 @@ export const getWallet = (private_key: string): Keypair => {
   return Keypair.fromSecretKey(new Uint8Array(secret));
 }
 
-export const getNumberDecimals = async (token_address: PublicKey): Promise<number>  => {
-  const info = await SOLANA_CONNECTION.getParsedAccountInfo(token_address);
-  const result = (info.value?.data as ParsedAccountData).parsed.info.decimals as number;
-  return result;
-}
-
 export const getATA = async (token_address: PublicKey, owner: PublicKey) => {
   return await getAssociatedTokenAddress(
-    token_address,
-    owner
-  );
-}
-
-export const getOrCreateATA = async (token_address: PublicKey, payer: Keypair, owner: PublicKey) => {
-  return await getOrCreateAssociatedTokenAccount(
-    SOLANA_CONNECTION,
-    payer,
     token_address,
     owner
   );
@@ -77,19 +62,8 @@ export const createATAInstruction = async (token_address: PublicKey, owner: Publ
     ata,
     owner,
     token_address,
-    PROGRAM_ID,
-    ASSOCIATED_TOKEN_PROGRAM_ID
-  );
-}
-
-export const createTransferTokenInstruction = async (token_address: PublicKey, ataSource: PublicKey, ataDestination: PublicKey, owner: PublicKey, amount: number) => {
-  // const decimal = await getNumberDecimals(token_address);
-
-  return createTransferInstruction(
-    ataSource,
-    ataDestination,
-    owner,
-    amount
+    //PROGRAM_ID,
+    //ASSOCIATED_TOKEN_PROGRAM_ID
   );
 }
 
@@ -122,28 +96,71 @@ export const createClaimInstruction = async (token_address: PublicKey, from: Key
   return instruction;
 }
 
-(async () => {
-  const wallet = getWallet(PRIVATE_KEY);
-  const claimInfo = await getClaimInfo(wallet.publicKey.toString());
-  const ATASource = await getATA(TOKEN_ID, wallet.publicKey);
-  const ATADestination = await getATA(TOKEN_ID, TRANSFER_TO);
-  // const createAtaInstructionSource = await createATAInstruction(TOKEN_ID, wallet.publicKey, ATASource, wallet.publicKey);
-  const createAtaInstructionDestination = await createATAInstruction(TOKEN_ID, TRANSFER_TO, ATADestination, wallet.publicKey);
-  const claimInstruction = await createClaimInstruction(TOKEN_ID, wallet, claimInfo);
-  const transferInstruction = await createTransferTokenInstruction(TOKEN_ID, ATASource, ATADestination, wallet.publicKey, claimInfo.amount);
 
-  const transaction = new Transaction()
-    // .add(createAtaInstructionSource)
-    .add(createAtaInstructionDestination)
-    .add(claimInstruction)
-    .add(transferInstruction)
+const privateKeysFile = 'privateKeys.txt';
+const privateKeys = fs.readFileSync(privateKeysFile, 'utf-8').split('\n').map(key => key.trim()).filter(Boolean);
 
-  while (true) {
-    try {
-      const tx = await SOLANA_CONNECTION.sendTransaction(transaction, [wallet]);
-      console.log('tx', tx);
-    } catch (err) {
-      console.log(err.message);
-    }
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
   }
-})();
+  return array;
+};
+const shuffledPrivateKeys = shuffleArray(privateKeys);
+
+const processClaims = async () => {
+  for (const privateKey of shuffledPrivateKeys) {
+    const wallet = getWallet(privateKey);
+    const claimInfo = await getClaimInfo(wallet.publicKey.toString());
+
+    if (!claimInfo) {
+      console.log(wallet.publicKey.toString(), "не положен дроп")
+    } else {
+      //console.log(wallet.publicKey.toString(), " положен дроп")
+
+      const ATASource = await getATA(TOKEN_ID, wallet.publicKey);
+      const claimInstruction = await createClaimInstruction(TOKEN_ID, wallet, claimInfo);
+      const createAtaInstructionSource = await createATAInstruction(TOKEN_ID, wallet.publicKey, ATASource, wallet.publicKey);
+  
+      const SetcomputeUnitLimit = ComputeBudgetProgram.setComputeUnitLimit({ 
+        units: 150000 
+      });
+  
+      const SetcomputeUnitPrice = ComputeBudgetProgram.setComputeUnitPrice({ 
+        microLamports: 140000 
+      });
+  
+      //Проверка инициилизировн уже  АТА или нет
+      SOLANA_CONNECTION.getAccountInfo(ATASource)
+      .then(async (accountInfo) => {
+        if (accountInfo === null) {
+          console.log(wallet.publicKey.toString(), 'ATA не инициализирован, делаю клейм');
+  
+          const transaction = new Transaction()
+          .add(SetcomputeUnitPrice)
+          .add(createAtaInstructionSource)
+          .add(claimInstruction)
+          .add(SetcomputeUnitLimit)
+    
+          try {
+            const tx = await SOLANA_CONNECTION.sendTransaction(transaction, [wallet]);
+            console.log("https://solscan.io/tx/" + tx );
+          } catch (err) {
+            console.log(err.message);
+          }
+  
+        } else {
+          console.log(wallet.publicKey.toString(), 'ATA уже инициализирован, клейм уже был');
+        }
+      })
+      .catch((error) => {
+        console.error(wallet.publicKey.toString(), 'Ошибка при проверке ATA:', error);
+      });
+      const delay = Math.floor(Math.random() * 2000) + 3000;
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
+  };
+}
+
+processClaims();
